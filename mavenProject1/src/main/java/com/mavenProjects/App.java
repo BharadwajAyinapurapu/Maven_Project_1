70d178923ba1e64d2d142deb5262abae22c573da
BharadwajAyinapurapu <bdisforall@gmail.com>
2022-07-17 11:53:09 +0530

    commit for calculator program

--- mavenProject1/src/main/java/com/mavenProjects/App.java
+++ mavenProject1/src/main/java/com/mavenProjects/App.java
@@ -4,28 +4,33 @@ package com.mavenProjects;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
 	public static void main(String[] args){
-	App obj =new App();
-	System.out.println(obj.evaluate(5,4));
+//		App obj =new App();
+//		System.out.println(obj.evaluate(5,4));
 	}
-    static int evaluate(int a,int b){
-	return a+b;
-    }
-}
-
-
-/*public class App{
-	
 	static int add(int a,int b){
 		return a+b;
 	}
-	static int sub(int a , int b){
+	static int sub(int a, int b){
 		return a-b;
 	}
 	static int mul(int a, int b){
 		return a*b;
 	}
-}*/
-		
+	static int div(int a, int b) {
+		if(b==0){
+			System.out.println("Divisor can't be zero");
+			return Integer.MIN_VALUE;
+		}
+		return a/b;
+	}
+	static boolean equalInteger(int a, int b){
+		boolean result = false;
+		if(a==b){
+			result=true;
+		}
+		return result;
+	}
+}	
--- mavenProject1/src/test/java/com/mavenProjects/AppTest.java
+++ mavenProject1/src/test/java/com/mavenProjects/AppTest.java
@@ -15,33 +15,63 @@ public class AppTest
     extends TestCase
 {
     	App obj = new App();
-
-	public void test1() {
-		int actual=obj.evaluate(10,20);
+	
+	//@Before
+	public void before(){
+		System.out.println("This is executed before each test");
+	}
+	
+	//@After
+	public void after(){
+		System.out.println("This is executed after each test");
+	}
+	
+	public void testSum1() {
+		int actual=obj.add(10,20);
 		int expected=30;
 		assertEquals(expected,actual);
 	}
 	
-	public void test2(){
-		int actual=obj.evaluate(0,0);
-		int expected=0;
-		assertEquals(expected,actual);
-	}
-	
-	public void test3(){
-		int actual=obj.evaluate(-10,-20);
+	public void testSum2(){
+		int actual=obj.add(-10,-20);
 		int expected=-30;
 		assertEquals(expected,actual);
 	}
 	
-	public void test4(){
-		int actual=obj.evaluate(-10,90);
-		int expected=80;
+	public void testSub1(){
+		int actual=obj.sub(-10,-20);
+		int expected=10;
 		assertEquals(expected,actual);
 	}
-	public void test5(){
-		int actual=obj.evaluate(560,97950);
-		int expected=98510;
+	
+	public void testMul1(){
+		int actual=obj.mul(-10,90);
+		int expected=-900;
 		assertEquals(expected,actual);
+	}
+	public void testDiv1(){
+		try{
+			int actual=obj.div(10,2);
+			int expected=5;
+			assertEquals(expected,actual);
+		}catch(Exception e){
+			e.printStackTrace(System.err);
+		}
+	}
+	
+	public void testDiv2(){
+		int actual=obj.div(10,0);
+		int expected=Integer.MIN_VALUE;
+		assertEquals(expected,actual);
+	}
+	public void testEqual1(){
+		boolean actual=obj.equalInteger(15,4);
+		boolean expected=false;
+		assertEquals(actual,expected);
+	}
+	public void testEqual2(){
+		boolean actual=obj.equalInteger(4,4);
+		boolean expected=true;
+		assertEquals(actual,expected);
 	}
 }
