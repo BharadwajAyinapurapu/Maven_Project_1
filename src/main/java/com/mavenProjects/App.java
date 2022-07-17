6893795b9aea313b13dc7c37735c81c1fb30b86e
BharadwajAyinapurapu <bdisforall@gmail.com>
2022-07-17 20:03:49 +0530

    update

--- src/main/java/com/mavenProjects/App.java
+++ src/main/java/com/mavenProjects/App.java
@@ -8,29 +8,30 @@ public class App
 {
 	public static void main(String[] args){
 		App obj =new App();
+		System.out.println("HI BHARADWAJ");
 		System.out.println(obj.add(5,4));
 		System.out.println(obj.sub(5,4));
 		System.out.println(obj.mul(5,4));
 		System.out.println(obj.div(5,4));
 		System.out.println(obj.equalInteger(5,4));
 	}
-	static int add(int a,int b){
+	 int add(int a,int b){
 		return a+b;
 	}
-	static int sub(int a, int b){
+	 int sub(int a, int b){
 		return a-b;
 	}
-	static int mul(int a, int b){
+	 int mul(int a, int b){
 		return a*b;
 	}
-	static int div(int a, int b) {
+	 int div(int a, int b) {
 		if(b==0){
 			System.out.println("Divisor can't be zero");
 			return Integer.MIN_VALUE;
 		}
 		return a/b;
 	}
-	static boolean equalInteger(int a, int b){
+	 boolean equalInteger(int a, int b){
 		boolean result = false;
 		if(a==b){
 			result=true;
